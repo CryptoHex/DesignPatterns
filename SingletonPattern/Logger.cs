f4615bc2f1707f84dc8b5dbc6f08d6c8cee18832
mSoftBg <mario.pavlov.pavlov@gmail.com>
2018-02-14 11:36:22 +0200

    Adding the Singleton Pattern

---
+++ SingletonPattern/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPattern
+{
+    public class Logger
+    {
+        private static Logger logger;
+
+        private Logger()
+        {
+
+        }
+
+        public static Logger GetInstance()
+        {
+            return logger ?? (logger = new Logger());
+        }
+
+        public void Write(string content)
+        {
+            Console.WriteLine(content);
+        }
+    }
+}
---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger log = Logger.GetInstance();
+            log.Write("This is the Singleton pattern and it can only have one instance");
+        }
+    }
+}
